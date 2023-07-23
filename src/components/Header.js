aa2bdb9cce95df8fa449d4dd5f87af6971bd0b26
arefiture <140311005+arefiture@users.noreply.github.com>
2023-07-23 16:06:14 +0500

    StartProject

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://ifab.se/wp-content/uploads/2019/12/Your_Logo-OUR-PRODUCTS.png' />
+        </header>
+    );
+}
+
+export default Header;
